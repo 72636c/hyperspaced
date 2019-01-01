0143edf3279037a95bffbd55e252ff06eadb2dca
Ryan Ling <ryan@outlook.com.au>
2019-01-01 13:21:53 +1100

    Add examples

---
+++ hyperspaced_doc_test.go
@@ -0,0 +1,11 @@
+package hyperspaced
+
+func ExampleSpaced() {
+	Spaced("AESTHETIC")
+	// Output: A E S T H E T I C
+}
+
+func ExampleSpacedN() {
+	SpacedN("AESTHETIC", 2)
+	// Output: A  E  S  T  H  E  T  I  C
+}
