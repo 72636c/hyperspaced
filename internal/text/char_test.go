b783eba2247b2187f6d9d3e69b7615eda8f0cc71
Ryan Ling <ryan@outlook.com.au>
2019-03-23 21:11:55 +1100

    chore(code): Run gofmt -s

--- internal/text/char_test.go
+++ internal/text/char_test.go
@@ -21,46 +21,46 @@ func Test_SplitString(t *testing.T) {
 		{
 			description: "One Letter",
 			input:       "f",
-			expected:    []text.Char{text.Char{'f'}},
+			expected:    []text.Char{{'f'}},
 		},
 		{
 			description: "Two Letters",
 			input:       "hi",
-			expected:    []text.Char{text.Char{'h'}, text.Char{'i'}},
+			expected:    []text.Char{{'h'}, {'i'}},
 		},
 		{
 			description: "Accent Normalisation",
 			input:       "e\u0301",
-			expected:    []text.Char{text.Char{'é'}},
+			expected:    []text.Char{{'é'}},
 		},
 		{
 			description: "Separate Emoji",
 			input:       "\U0001f30f\u2696",
-			expected:    []text.Char{text.Char{'🌏'}, text.Char{'⚖'}},
+			expected:    []text.Char{{'🌏'}, {'⚖'}},
 		},
 		{
 			// https://emojipedia.org/family-man-light-skin-tone-woman-light-skin-tone-girl-light-skin-tone-baby-light-skin-tone/
 			description: "Sequenced Emoji",
 			input:       "\U0001f468\U0001f3fb\u200d\U0001f469\U0001f3fb\u200d\U0001f467\U0001f3fb\u200d\U0001f476\U0001f3fb",
-			expected:    []text.Char{text.Char{'👨', '🏻', '\u200d', '👩', '🏻', '\u200d', '👧', '🏻', '\u200d', '👶', '🏻'}},
+			expected:    []text.Char{{'👨', '🏻', '\u200d', '👩', '🏻', '\u200d', '👧', '🏻', '\u200d', '👶', '🏻'}},
 		},
 		{
 			// https://emojipedia.org/female-sleuth/
 			description: "Female Variant Selector",
 			input:       "\U0001f575\ufe0f\u200d\u2640\ufe0f",
-			expected:    []text.Char{text.Char{'🕵', '\ufe0f', '\u200d', '♀', '\ufe0f'}},
+			expected:    []text.Char{{'🕵', '\ufe0f', '\u200d', '♀', '\ufe0f'}},
 		},
 		{
 			// https://emojipedia.org/female-technologist/
 			description: "Join Control",
 			input:       "\U0001f469\u200d\U0001f4bb",
-			expected:    []text.Char{text.Char{'👩', '\u200d', '💻'}},
+			expected:    []text.Char{{'👩', '\u200d', '💻'}},
 		},
 		{
 			// https://emojipedia.org/man-type-6/
 			description: "Skin Tone Modifier",
 			input:       "\U0001f468\U0001f3ff",
-			expected:    []text.Char{text.Char{'👨', '🏿'}},
+			expected:    []text.Char{{'👨', '🏿'}},
 		},
 	}
 
